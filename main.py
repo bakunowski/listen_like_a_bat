f7f78bfe8d092cbbe6116f3cf048d865ce4316e1
Karol Bakunowski <bakunowskikarol@gmail.com>
2020-05-27 16:04:23 +0100

    added calculation of convolved IRs with the call

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import utils as u
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 
-datapath = current_path + \
-    '/FlowerClassification/FlowerIRs/Pguian_h_impuls_bin/Pguian h I 20cm_2009-06-03_NA_2019-08-20_impuls.csv'
-
 folderpath = current_path + '/FlowerClassification/FlowerIRs/'
 fingerprints = current_path + '/EchoFingerprints/'
+conv_spectrograms = current_path + '/ConvolvedSpectrograms/'
 call_datapath = current_path + \
     '/FlowerClassification/EcholocationCalls/Glosso_call.wav'
 
@@ -15,6 +13,9 @@ for dirpath, dirnames, filenames in os.walk(folderpath):
     for filename in [f for f in filenames if f.endswith(".csv")]:
         print('Getting fingerprint for:', filename)
         data = u.load_data(dirpath + '/' + filename, show_head=False)
-        call, _, _ = u.return_bat_calls(call_datapath, plot=False)
-        u.retrieve_fingerprint(data, call, fingerprints + filename)
-        break
+        call, call2, _ = u.return_bat_calls(call_datapath, plot=False)
+        # u.retrieve_fingerprint(data, call2, fingerprints + filename)
+
+        a = filename.split()
+        concatname = a[0] + '_' + a[1] + '_' + a[2]
+        u.get_convolved_call(data, call2, conv_spectrograms + concatname)
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import librosa.display
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import scipy.signal as sp
+from scipy import signal
 
 
 def calculate_spectrogram(y, window_size, hop_size):
@@ -79,26 +79,64 @@ def load_data(path_to_csv, show_head=False):
     return data
 
 
+def get_kHz_scale_vec(ks, sample_rate, Npoints):
+    freq_kHz = (ks*sample_rate/Npoints) // 1000
+    freq_kHz = [int(i) for i in freq_kHz]
+    return(freq_kHz)
+
+
 def retrieve_fingerprint(data, call, name):
     data = data.to_numpy()
-    # n_fft = 65536
-    n_fft = 131072
-    hop_size = n_fft//32
+    n_fft = 4096
+    hop_size = n_fft//2
     x = n_fft // 2
     y = 1
     zeros = np.zeros((101, x, y))
 
     for i, ir in enumerate(data):
         # echo fingerprint
-        zeros[i], _ = calculate_spectrogram(np.trim_zeros(ir), n_fft, hop_size)
-
-        # bat call over fingerprint
-        # echo = np.convolve(np.trim_zeros(ir), np.trim_zeros(call))
-        # zeros[i], _ = calculate_spectrogram(np.trim_zeros(echo), n_fft, hop_size)
+        zeros[i], _ = calculate_spectrogram(ir, n_fft, hop_size)
 
     spec = zeros.reshape(101, x*y)
 
+    plt.rcParams.update({'font.size': 12})
+
+    ks = np.linspace(0, spec.shape[1], 15)
+    ksHz = get_kHz_scale_vec(ks, 500000, n_fft*y)
+    plt.yticks(ks, ksHz)
+    plt.ylabel('Frequency (kHz)')
+
+    t = np.linspace(0, 100, 7)
+    my_ticks_x = [-90, -60, -30, 0, 30, 60, 90]
+    plt.xticks(t, my_ticks_x)
+    plt.xlabel('angle of sound incidence (°)')
+
+    axes = plt.gca()
+    axes.set_xlim([0, None])
+    axes.set_ylim([280, 1320])
+
     plt.imshow(np.abs(spec.T), aspect='auto',
                origin='lower')
-    plt.savefig('%s.png' % name, dpi=96)
+    plt.show()
+    # plt.savefig('%s.png' % name, dpi=96)
 
+def get_convolved_call(data, call, name):
+    data = data.to_numpy()
+    perseg = 256
+    starting_angle = -90
+
+    for ir in data:
+        # bat call over each ir
+        echo = np.convolve(call, ir) 
+        f, t, spec = signal.spectrogram(np.trim_zeros(echo), fs=500000, window='hann', nperseg=perseg,
+                                        noverlap=perseg-1, detrend=False, scaling='spectrum')
+
+        plt.rcParams.update({'font.size': 12})
+
+        # plt.pcolormesh(t, f / 1000, spec, cmap='magma')
+        plt.pcolormesh(t, f / 1000, 10 *  np.log10(spec))
+        plt.ylabel('Frequency [kHz]')
+        plt.xlabel('Time [s]')
+        plt.show()
+        # plt.savefig('%s_Glosso_%s' % (name, round(starting_angle)), dpi=96)
+        # starting_angle += 1.8
