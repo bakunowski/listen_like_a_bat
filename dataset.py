9c89d73a75a9d8eb2fd2d601d8374f45a98cb4b5
Karol Bakunowski <bakunowskikarol@gmail.com>
2020-06-22 20:48:59 +0100

    changes accomodating full dataset

--- dataset.py
+++ dataset.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import librosa
 from scipy import signal
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from random import randint
 import matplotlib.pyplot as plt
 
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 labelscv = "/home/bakunowski/Documents/QueenMary/FinalProject/listen_like_a_bat/listen_like_a_bat.csv"
 
 # Path to folder with folders containing .csv files with impulse responses
-folder = "/home/bakunowski/Documents/QueenMary/FinalProject/listen_like_a_bat/"
+folder = "/home/bakunowski/Documents/QueenMary/FinalProject/listen_like_a_bat/Data"
 
 # Path to bat call
 call_datapath = "/home/bakunowski/Documents/QueenMary/FinalProject/listen_like_a_bat/FlowerClassification/EcholocationCalls/Glosso_call.wav"
@@ -49,13 +49,13 @@ class Echoes(Dataset):
         return len(index)
 
     def load_data(self, path_to_csv):
-        data = pd.read_csv(path_to_csv, sep='\t', header=None)
+        data = pd.read_csv(path_to_csv, sep=';', header=None)
         data = data.fillna(0)
         data = data.to_numpy()
         return data
 
     def get_echo(self, idx, plot=False):
-        ir_csv = self.load_data(folder + self.plant_name.iloc[idx, 0])
+        ir_csv = self.load_data(folder + '/' + self.plant_name.iloc[idx, 0])
         perseg = 256
         angle = idx * angle_calc_multiplier
 
@@ -63,7 +63,7 @@ class Echoes(Dataset):
         echo = np.convolve(self.bat_call, ir_csv[0])
         f, t, spec = signal.spectrogram(echo, fs=500000,
                                         window='hann', nperseg=perseg,
-                                        noverlap=perseg-1, detrend=False,
+                                        noverlap=perseg-5, detrend=False,
                                         scaling='spectrum')
 
         if plot:
@@ -111,10 +111,32 @@ class Echoes(Dataset):
         return label, echo
 
 
-echoes_dataset = Echoes(labelscv, call_datapath, maxfilestoload)
-print(len(echoes_dataset))
+# little helper to show current batch of echoes
+def show_echo_batch(label, echoes):
+    fig = plt.figure()
 
-train_loader = DataLoader(echoes_dataset, batch_size=4, shuffle=True)
+    for i in range(len(echoes)):
+
+        ax = plt.subplot(1, 4, i + 1)
+        plt.tight_layout()
+        ax.set_title('Sample #{}'.format(i))
+        ax.axis('off')
+
+        spec_dB = 10 * np.log10(echoes[i])
+        spec_min, spec_max = -100, -20
+
+        plt.pcolormesh(spec_dB, vmin=spec_min, vmax=spec_max)
+
+    plt.show()
+
+
+echoes_dataset = Echoes(labelscv, call_datapath, maxfilestoload)
+
+trainset, valset = random_split(echoes_dataset, [200, 34])
+
+train_loader = DataLoader(trainset, batch_size=4, shuffle=True)
+val_loader = DataLoader(valset, batch_size=4, shuffle=True)
 
 for i_batch, sample_batched in enumerate(train_loader):
     print(i_batch, sample_batched[1].size(), sample_batched[0].size())
+    # show_echo_batch(sample_batched[0], sample_batched[1])
